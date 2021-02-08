9a73407118c520e784c7186a0f535704b6f5f77a
talryan <nataliamryan@gmail.com>
2021-02-07 23:20:38 -0800

    commit tree ec78a1df9e5d0dd0b3e07428b92b34d6fa467886

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000 ) + 1;
+
+const num3 = 8;
+const num4 = 4;
+
+const mod = num3 - num4;
+
+const max = Math.max(1, 2, 3, 4, 20);
